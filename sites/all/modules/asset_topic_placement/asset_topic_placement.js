415d8791b5499971c58df0daf429215b20be1a2f
Dale Frey <dfrey@ctacorp.com>
2015-01-22 12:43:30 -0500

    updating Assest Topic Placement module again

--- sites/all/modules/asset_topic_placement/asset_topic_placement.js
+++ sites/all/modules/asset_topic_placement/asset_topic_placement.js
@@ -1,11 +1,19 @@
 
+// On document ready, we may need to initialize this helper script
 jQuery(document).ready( function () {
 
+	/*Every so often, check if we need to initialize this helper script [again]
+	This is necessary because a single term's edit form can be loaded on a 
+	single page multiple times in the "Taxonomy Manager" */
 	setInterval(function () {
 		
+		// If there is a term's edit form on this page, and this help script has NOT been applied to it...
 		if ( jQuery('.group-asset-topic-placement').length > 0 && jQuery('.group-asset-topic-placement.helper-script-applied').length == 0 ) {
+
 			console.log('Firing initAssetTopicPlacementHelperScript()');
 			initAssetTopicPlacementHelperScript();
+
+			// Note this helper-script has been applied to this form
 			jQuery('.group-asset-topic-placement').addClass('helper-script-applied');
 		}
 
@@ -24,13 +32,22 @@ function initAssetTopicPlacementHelperScript() {
 		setTimeout( function (tThis) {
 
 			if ( tThis.checked ) {
-				jQuery('.group-asset-topic-placement').queue( function () {
-					jQuery('.group-asset-topic-placement').fadeIn();
-					alterTermsOnAssetTopicPlacementFields([tThis.value], [], function () {
-						updateAssetTopicPlacementCountClasses();
-						jQuery('.group-asset-topic-placement').dequeue();
+				if ( jQuery('.group-asset-topic-placement').queue('fx').length < 3 ) {
+					jQuery('.group-asset-topic-placement').queue( function () {
+
+						jQuery('.group-asset-topic-placement').fadeIn();
+
+						var addTids = [];
+						jQuery('.field-name-field-asset-topic-taxonomy input:checked').each( function () {
+							addTids.push(this.value);
+						});
+
+						alterTermsOnAssetTopicPlacementFields(addTids, [], function () {
+							updateAssetTopicPlacementCountClasses();
+							jQuery('.group-asset-topic-placement').dequeue();
+						});
 					});
-				});
+				}
 			} else {
 				jQuery('.group-asset-topic-placement').queue( function () {
 					jQuery('.group-asset-topic-placement input[value=' + tThis.value + ']').parents('tr').remove();
@@ -43,8 +60,16 @@ function initAssetTopicPlacementHelperScript() {
 
 	});
 
+	// When the user clicks an "Inherit" checkbox...
+	jQuery('.group-asset-topic-placement input[name*="inherit"]').bind('click', function () {
+		// I'm using setTimeout() to make [very] sure that this event fires AFTER the browser has handled the checkbox ticked-value alteration...
+		setTimeout( function () {
+			enforceAssetTopicOrderVisibilityBasedOnInheritance();
+		}, 10);
+	});
+
 	/* When the page first loads, we want to make sure any [currently] selected term under 
-	"Asset Topic Taxonomy" shows under ALL lists in "Asset Topic Placement" (this is nessesary 
+	"Asset Topic Taxonomy" shows under ALL lists in "Asset Topic Placement" (this is necessary 
 	on taxonomy edit-pages) */
 	var tickedCheckboxes = jQuery('.field-name-field-asset-topic-taxonomy input:checked');
 	if ( tickedCheckboxes.length > 0 ) {
@@ -58,8 +83,30 @@ function initAssetTopicPlacementHelperScript() {
 	}
 }
 
+/* Shows or hides each "Asset Topic Order" field based on weather to not 
+ * the "Inherit this region's assets from parent" sibling-checkbox is ticked.
+ */
+function enforceAssetTopicOrderVisibilityBasedOnInheritance() {
+
+	
+	jQuery('.group-asset-topic-placement .form-item.form-type-checkbox label:contains("Inherit")').each( function () {
+		var jqThis = jQuery(this);
+		var inheritCheckbox = jqThis.siblings('input');
+		var fieldsetContainer = jqThis.parents('.fieldset-wrapper').eq(0); // Search up the DOM from $(this) to the first div.fieldset-wrapper
+		var assetTopicOrderField = fieldsetContainer.children('.field-type-taxonomy-term-reference');
+
+		if ( inheritCheckbox.attr('checked') ) {
+			assetTopicOrderField.fadeOut();
+		} else {
+			assetTopicOrderField.fadeIn();
+		}
+	});
+
+}
+
 function updateAssetTopicPlacementCountClasses() {
 
+	// These classes are used for theming purposes in asset_topic_placement.css
 	if ( jQuery('.field-name-field-asset-topic-taxonomy input:checked').length > 0 ) {
 		jQuery('.group-asset-topic-placement').removeClass('term-count-0');
 		jQuery('.group-asset-topic-placement').addClass('term-count-not-0');
@@ -72,13 +119,13 @@ function updateAssetTopicPlacementCountClasses() {
 
 function alterTermsOnAssetTopicPlacementFields(addTermIDs, removeTermIDs, callback) {
 
-	jQuery('.group-asset-topic-placement').addClass('term-processing');
+	jQuery('.group-asset-topic-placement').addClass('term-processing'); // This shows a spinner
 
 	alterTermsOnAssetTopicPlacementField(addTermIDs, removeTermIDs, '.field-name-field-asset-topic-carousel', function () {
 		alterTermsOnAssetTopicPlacementField(addTermIDs, removeTermIDs, '.field-name-field-asset-topic-sidebar', function () {
 			alterTermsOnAssetTopicPlacementField(addTermIDs, removeTermIDs, '.field-name-field-asset-topic-footer', function () {
 
-				jQuery('.group-asset-topic-placement').removeClass('term-processing');
+				jQuery('.group-asset-topic-placement').removeClass('term-processing'); // This removes the spinner
 
 				// Trigger callback
 				if ( typeof callback === 'function' ) {
