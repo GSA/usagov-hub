b0657988d16df75cfefadd044956812dcc92ef0b
Dale Frey <dfrey@ctacorp.com>
2015-05-11 12:09:37 -0400

    making some changes to the HR module (changes I always meant to do in BUSA). This is actually something I need to do anyways as it is getting in my way in completing ticket usagov-100287 (I cant use hook_module_implements_alter in reaction files before)

--- sites/all/modules/hooks_reaction/reactions.php
+++
@@ -1,9 +0,0 @@
-<?php /*
-
-    [!!] DO NOT TOUCH THIS FILE [!!]
-
-    THIS FILE IS DEPRECATED - DO NOT ADD ANYTHING TO THIS FILE
-    Instead, add new files into the reactions directory.
-
-    [!!] DO NOT TOUCH THIS FILE [!!]
-*/
