7249124cf5c5a0e926b9e4af12d225eb458c8c68
Dale Frey <dfrey@ctacorp.com>
2015-09-02 15:29:58 -0400

    same

--- sites/all/modules/hooks_reaction/reactions/debug.php
+++ sites/all/modules/hooks_reaction/reactions/debug.php
@@ -7,11 +7,19 @@ hooks_reaction_add('HOOK_node_presave',
             return;
         }
 
+        $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        foreach ( $bt as &$entry ) {
+            unset($entry['object']);
+        }
+        dsm($bt);
+        $bt = json_decode(json_encode($bt), true);
+        dsm($bt);
+
         $dalenode = variable_get('dalenode', array());
         $dalenode[] = array(
             'pid' => getmypid(),
             'server' => $_SERVER,
-            'backtrace' => json_decode(json_encode(debug_backtrace())),
+            'thebacktrace' => $bt,
             'node' => json_decode(json_encode($node))
         );
         variable_set('dalenode', $dalenode);
