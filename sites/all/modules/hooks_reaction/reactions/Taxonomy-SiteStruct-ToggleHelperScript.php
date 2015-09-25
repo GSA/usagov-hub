6d71f8aaa5fa3596949a6fc38a730dd166640a2e
Dale Frey <dfrey@ctacorp.com>
2015-09-25 12:29:18 -0400

    usagov-100592

--- sites/all/modules/hooks_reaction/reactions/Taxonomy-SiteStruct-ToggleHelperScript.php
+++ sites/all/modules/hooks_reaction/reactions/Taxonomy-SiteStruct-ToggleHelperScript.php
@@ -46,51 +46,31 @@ hooks_reaction_add('HOOK_taxonomy_term_presave',
         // Get the target referenced/toggle term-ID
         $targetRelationTermId = _taxSiteStructToggleHelper_getRefToggleTerm($term);
 
+        // If the "English/Spanish Toggle" is empty, then empty out the "USA.gov Toggle URL" and "GobiernoUSA.gov Toggle URL" fields
+        if ( $targetRelationTermId == false ) {
+            $term->field_usa_gov_toggle_url = array();
+            $term->field_gobiernousa_gov_toggle_url = array();
+        }
+
         // We only act when there is data supplied in the field_english_spanish_toggle field
         if ( $targetRelationTermId == false ) {
             return;
         }
 
-        /* Before we implement a 2-way relation between two terms, let us check to 
-        see if the other term is already related to something else */
-        $targetTermRelationCount = db_query("
-            SELECT COUNT(*) AS 'count'
-            FROM field_data_endpoints e1 
-            LEFT JOIN field_data_endpoints e2 ON ( e1.entity_id = e2.entity_id )
-            WHERE 
-                e1.bundle = 'language_toggle'
-                AND e1.endpoints_entity_id = {$targetRelationTermId}
-                AND NOT ( 
-                    e2.endpoints_entity_id IN ({$term->tid}, {$targetRelationTermId}) 
-                    AND e2.endpoints_entity_id IN ({$term->tid}, {$targetRelationTermId}) 
-                )
-        ")->fetchColumn();
-
-        // If the other term IS related to something else, clobber it
-        if ( intval($targetTermRelationCount) > 0 ) {
-
-            error_log("SiteStruct-ToggleHelper wants to associate a term with another therm, that is already "
-                ."associated with something else. SiteStruct-ToggleHelper is now deleting relations in its way.");
-            
-            $results = db_query("
-                SELECT DISTINCT e1.entity_id AS 'relationid'
-                FROM field_data_endpoints e1 
-                LEFT JOIN field_data_endpoints e2 ON ( e1.entity_id = e2.entity_id )
-                WHERE 
-                    e1.bundle = 'language_toggle'
-                    AND e1.endpoints_entity_id = {$targetRelationTermId}
-                    AND NOT ( 
-                        e2.endpoints_entity_id IN ({$term->tid}, {$targetRelationTermId}) 
-                        AND e2.endpoints_entity_id IN ({$term->tid}, {$targetRelationTermId}) 
-                    )
-            ");
-
-            foreach ( $results as $result ) {
-                relation_delete($result->relationid);
-                error_log("Deleted relation ".$result->relationid);
-            }
+        // If this $term alread has a DIFFERENT relation to something else, kill it.
+        $killOldRelation = _taxSiteStructToggleHelper_getOldRelationToDelete($term->tid, array($term->tid, $targetRelationTermId));
+        if ( $killOldRelation ) {
+            error_log("SiteStruct-ToggleHelper killing relation in the way: ".$killOldRelation);
+            relation_delete($killOldRelation);
         }
-        
+
+        // And we shall do the same for the target/related term
+        $killOldRelation = _taxSiteStructToggleHelper_getOldRelationToDelete($targetRelationTermId, array($term->tid, $targetRelationTermId));
+        if ( $killOldRelation ) {
+            error_log("SiteStruct-ToggleHelper killing relation in the way: ".$killOldRelation);
+            relation_delete($killOldRelation);
+        }
+
         // We now must determin which field we are going to update; the "USA.gov Toggle URL" or "GobiernoUSA.gov Toggle URL".
         $targetIdUnderTree = db_query("SELECT tlt_name FROM taxonomy_tlt_name WHERE tid = {$targetRelationTermId}")->fetchColumn();
         switch ( $targetIdUnderTree ) {
@@ -184,64 +164,6 @@ hooks_reaction_add('HOOK_taxonomy_term_presave',
 ); // End hooks_reaction_add() call
 
 
-/** 
- * int _taxSiteStructToggleHelper_getRefToggleTerm(object $term)
- * int _taxSiteStructToggleHelper_getRefToggleTerm(int $term)
- *
- * This function will look at a given $term, and return the term-ID for the 
- * target toggle-term, referenced in the field_english_spanish_toggle field.
- */
-function _taxSiteStructToggleHelper_getRefToggleTerm($term ) {
-
-    // If a term-ID was passed, load the term
-    if ( !is_object($term) && is_numeric($term) ) {
-        $term = taxonomy_term_load($term);
-    }
-
-    /* There may or may not be a $term->field_english_spanish_toggle['und'][0]['endpoints'] array.
-    this array exsists AFTER the term AND relation is fully saved into the database. We will first
-    try to check this, should it exsist.  */ 
-    if ( !empty($term->field_english_spanish_toggle['und'][0]['endpoints']) ) {
-
-        $endpoints = $term->field_english_spanish_toggle['und'][0]['endpoints']['und'];
-
-        // We shall return the term-ID of the endpoint which is NOT this one ($term->tid)
-        foreach ( $endpoints as $endpoint ) {
-            if ( !empty($endpoint['entity_id']) && intval($endpoint['entity_id']) !== intval($term->tid) ) {
-
-                // then we have found an endpoint that is not this $term->tid
-                return $endpoint['entity_id'];
-            }
-        }
-    }
-
-    // if this line is hit (if we havent returned yet), then the $term->[...][endpoints] are not available yet
-
-    /* The und/0/relation_options/targets/target_2 path may contain a string like:
-            Animal Keeper - (site_strucutre_taxonomy) [taxonomy_term:10354]
-            or: Animal Keeper [taxonomy_term:10354]
-
-        While you dont see data in this [object] path on the Devel-tab of any term, you will 
-        see this data exist if you were to place a dsm() call in this function here (during a 
-        term-save).
-    */
-
-    // We must dissect the term-id out of the string at: und/0/relation_options/targets/target_2
-    $targetRelationString = $term->field_english_spanish_toggle['und'][0]['relation_options']['targets']['target_2'];
-    $words = explode(':', $targetRelationString);
-    $lastWord = @array_pop($words);
-    $targetRelationTermId = intval($lastWord);
-
-    // Bail if we failed to parse out the target-id
-    if ( $targetRelationTermId === 0 ) {
-        error_log('Error - Taxonomy-SiteStruct-ToggleHelperScript.php failed to obtain a target reference-id.');
-        return false;
-    }
-
-    return $targetRelationTermId;
-}
-
-
 /**
  * Implements:  HOOK_taxonomy_term_save  -and-  HOOK_taxonomy_term_update
  * 
@@ -298,4 +220,130 @@ function _taxSiteStructToggleHelper_postTermSave_updateRefTerm() {
     taxonomy_term_save($term);
 
     $GLOBALS['taxSiteStrucTogHelper_noPostProc'] = false;
+}
+
+
+/** 
+ * int _taxSiteStructToggleHelper_getRefToggleTerm(object $term)
+ * int _taxSiteStructToggleHelper_getRefToggleTerm(int $term)
+ *
+ * This function will look at a given $term, and return the term-ID for the 
+ * target toggle-term, referenced in the field_english_spanish_toggle field.
+ */
+function _taxSiteStructToggleHelper_getRefToggleTerm($term ) {
+
+    // If a term-ID was passed, load the term
+    if ( !is_object($term) && is_numeric($term) ) {
+        $term = taxonomy_term_load($term);
+    }
+
+    /* There may or may not be a $term->field_english_spanish_toggle['und'][0]['endpoints'] array.
+    this array exsists AFTER the term AND relation is fully saved into the database. We will first
+    try to check this, should it exsist.  */ 
+    if ( !empty($term->field_english_spanish_toggle['und'][0]['endpoints']) ) {
+
+        $endpoints = $term->field_english_spanish_toggle['und'][0]['endpoints']['und'];
+
+        // We shall return the term-ID of the endpoint which is NOT this one ($term->tid)
+        foreach ( $endpoints as $endpoint ) {
+            if ( !empty($endpoint['entity_id']) && intval($endpoint['entity_id']) !== intval($term->tid) ) {
+
+                // then we have found an endpoint that is not this $term->tid
+                return $endpoint['entity_id'];
+            }
+        }
+    }
+
+    // if this line is hit (if we havent returned yet), then the $term->[...][endpoints] are not available yet
+
+    /* The und/0/relation_options/targets/target_2 path may contain a string like:
+            Animal Keeper - (site_strucutre_taxonomy) [taxonomy_term:10354]
+            or: Animal Keeper [taxonomy_term:10354]
+
+        While you dont see data in this [object] path on the Devel-tab of any term, you will 
+        see this data exist if you were to place a dsm() call in this function here (during a 
+        term-save).
+    */
+
+    if ( empty($term->field_english_spanish_toggle['und']) ) {
+        return false;
+    }
+
+    // We must dissect the term-id out of the string at: und/0/relation_options/targets/target_2
+    $targetRelationString = $term->field_english_spanish_toggle['und'][0]['relation_options']['targets']['target_2'];
+    $words = explode(':', $targetRelationString);
+    $lastWord = @array_pop($words);
+    $targetRelationTermId = intval($lastWord);
+
+    // Bail if we failed to parse out the target-id
+    if ( $targetRelationTermId === 0 ) {
+        error_log('Error - Taxonomy-SiteStruct-ToggleHelperScript.php failed to obtain a target reference-id. '
+            .'(note that this message may be a false-positive)');
+        return false;
+    }
+
+    return $targetRelationTermId;
+}
+
+
+/** 
+ * int _taxSiteStructToggleHelper_getOldRelationToDelete(object $term)
+ * int _taxSiteStructToggleHelper_getOldRelationToDelete(int $term)
+ *
+ * [!!] NOTICE [!!] - This function only looks after the field_english_spanish_toggle field.
+ *
+ * So there seems to be a bug with the relation module, the bug is;
+ *    If you relate node-A with node-B, with a 2-way relation, then node-B points to node-A as expected.
+ *    HOWEVER, if you then relate node-A with node-C...
+ *       node-A points to node-C, and node-C points to node-A, as expected
+ *       BUT: node-B will still be pointing to node-A
+ *       ALSO, the node-A <--> node-B relation will actually still be found in the database (in MySQL), but not seen on the interface
+ *
+ *    Interestingly, if you were to then delete the node-A <--> node-C relation, the node-A <--> node-B relations will
+ *    then magically be seen again in the interface.
+ *
+ *    In other words, it seems the relation module is not smart enough to clobber relations in its 
+ *    way when creating 2-way relations. Instead, it just appends a new relation, even though our 
+ *    field_english_spanish_toggle field in Drupal is set to only hold one value. With this bug,
+ *    the relations module essentially is is treating all relation fields as a multi-value field.
+ *
+ * This function will look at the given $term, and given two "new/expected" endpoints - should there be an 
+ * "old" relation already stored on $term, then the relation-id [to delete] will be returned.
+ *
+ * This function call is most usefull durring HOOK_*_presave.
+ *
+ */
+function _taxSiteStructToggleHelper_getOldRelationToDelete($term, $expectedEndpoints = array()) {
+
+    // If a term-ID was passed, load the term
+    if ( !is_object($term) && is_numeric($term) ) {
+        $term = taxonomy_term_load($term);
+    }
+
+    // Bail if there are no endpoints/references
+    if ( empty($term->field_english_spanish_toggle['und'][0]['endpoints']['und']) ) {
+        return;
+    }
+
+    // Get the referenced entities
+    $exsistingEndpoints = $term->field_english_spanish_toggle['und'][0]['endpoints']['und'];
+
+    // Bail if there are no endpoints/references
+    if ( !is_array($exsistingEndpoints) || count($exsistingEndpoints) == 0 ) {
+        return;
+    }
+
+    // Check each endpoint on this $term, trying to find one that is unexpected
+    foreach ( $exsistingEndpoints as $exsistingEndpoint ) {
+
+        // If this endpoint-target is unexpected...
+        if ( !in_array($exsistingEndpoint['entity_id'], $expectedEndpoints) ) {
+
+            // ...then this relation is unexpected
+            $relationId = $term->field_english_spanish_toggle['und'][0]['rid'];
+            return intval($relationId);
+        }
+    }
+
+    return false;
 }
