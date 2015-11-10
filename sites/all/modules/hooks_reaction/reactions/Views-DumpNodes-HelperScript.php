e35437539095a1c6a5c7611148ba9d16cdfb9ff2
Dan Narkiewicz <dnarkiewicz@ctacorp.com>
2015-11-10 14:06:52 -0500

    add relativeUrl conversion to absoluteUrl for api xml feed

--- sites/all/modules/hooks_reaction/reactions/Views-DumpNodes-HelperScript.php
+++ sites/all/modules/hooks_reaction/reactions/Views-DumpNodes-HelperScript.php
@@ -1,8 +1,8 @@
 <?php /*
 
     [--] PURPOSE [--]
-    
-    The purpose of this script is to assist the dump_nodes View with rendering - this View uses 
+
+    The purpose of this script is to assist the dump_nodes View with rendering - this View uses
     some custom functions that must be defined outside of the View.
 
     This helper script also assists in altering the query sent to MySQL for this View,
@@ -33,7 +33,7 @@ hooks_reaction_add("HOOK_views_pre_execute",
             $query->range(($page)*$limit, $limit);
 
             /* COMMENTING OUT AS THIS CODE DOES NOT BELONG HERE?
-            
+
                 // The "Recent Data export" display should only show taxonomy-terms that have been modded in the last 15mins
                 if ( $view->current_display === 'views_data_export_2' ) {
                     $query->leftJoin('taxonomy_dates', 'd', '(taxonomy_term_data.tid=d.tid)');
@@ -49,26 +49,77 @@ hooks_reaction_add("HOOK_views_pre_execute",
 if ( !class_exists('SimpleXMLExtended') ) {
     class SimpleXMLExtended extends SimpleXMLElement {
         public function addCData($cdata_text) {
-            $node = dom_import_simplexml($this); 
-            $no   = $node->ownerDocument; 
-            $node->appendChild($no->createCDATASection($cdata_text)); 
-        } 
+            $node = dom_import_simplexml($this);
+            $no   = $node->ownerDocument;
+            $node->appendChild($no->createCDATASection($cdata_text));
+        }
     }
+}
+
+function _vdn_forUseBy( $node )
+{
+  $by = [];
+  foreach ( ['field_for_use_by','field_for_use_by_text'] as $field )
+  {
+    if ( !empty($node->$field) )
+    {
+      foreach ( $node->$field as $l )
+      {
+        foreach ( $l as $fub )
+        {
+          if ( !empty($fub['value']) )
+          {
+            $by[strtolower($fub['value'])] = strtolower($fub['value']);
+          }
+        }
+      }
+    }
+  }
+  return $by;
+}
+function _vdn_absoluteLinks( &$node )
+{
+  $host = null;
+  $hosts = [
+       'usa.gov'          => 'https://www.usa.gov'
+      ,'gobiernousa.gov'  => 'https://gobierno.usa.gov'
+      ,'gobierno.usa.gov' => 'https://gobierno.usa.gov'
+      ,'kids.usa.gov'     => 'https://kids.usa.gov'
+      ,'blog.usa.gov'     => 'https://blog.usa.gov'
+  ];
+  $fubs = _vdn_forUseBy( $node );
+  foreach ( $fubs as $fub )
+  {
+    if ( isset($hosts[$fub]) )
+    {
+      $host = $hosts[$fub];
+      break;
+    }
+  }
+  $node->body['und'][0]['value'] = preg_replace(
+     "/(href|src)\s*\=\s*([\"'])\s*([^(https?|mailto|ftp)])/",
+     "$1=$2$host/$3", $node->body['und'][0]['value']);
+}
+function _vdn_deletionDetails( &$node )
+{
+  // override deleted, deletion_uid, deletion_timestamp
+  if (isset($node) && is_object($node)) {
+      $del = (db_query("SELECT deletion_uid, deletion_timestamp FROM node_deleted WHERE nid = :nid", array(":nid"=>$node->nid))->fetchAssoc());
+      if (isset($del) && !empty($del) && is_array($del)) {
+          $node->deleted=1;
+          $node->deletion_uid = $del['deletion_uid'];
+          $node->deletion_timestamp = $del['deletion_timestamp'];
+      }
+  }
 }
 
 function nidToXML($nid) {
 
     // initializing or creating array
     $n = node_load($nid);
-    // override deleted, deletion_uid, deletion_timestamp
-    if (isset($n) && is_object($n)) {
-        $del = (db_query("SELECT deletion_uid, deletion_timestamp FROM node_deleted WHERE nid = :nid", array(":nid"=>$nid))->fetchAssoc());
-        if (isset($del) && !empty($del) && is_array($del)) {
-            $n->deleted=1;
-            $n->deletion_uid = $del['deletion_uid'];
-            $n->deletion_timestamp = $del['deletion_timestamp'];
-        }
-    }
+
+    _vdn_deletionDetails( $n );
+    _vdn_absoluteLinks(   $n );
 
     $arrNodeData = json_decode( json_encode($n), true );
 
