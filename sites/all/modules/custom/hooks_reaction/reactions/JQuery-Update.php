e7be5bc6cd691a4beea7cbe6001e0ec60e5eefc3
Dan Narkiewicz <dnarkiewicz@ctacorp.com>
2017-11-14 15:06:16 -0500

    public pages get newer jquery

---
+++ sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
@@ -0,0 +1,11 @@
+<?php
+
+hooks_reaction_add("js_alter",
+    function (&$javascript) {
+      if ( !user_is_logged_in() && !empty($javascript['misc/jquery.js']) )
+      {
+        $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.2.1.min.js';
+        $javascript['misc/jquery.js']['version'] = '3.2.1';
+      }
+    }
+);
