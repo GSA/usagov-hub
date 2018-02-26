6ab1cf69466e87e3724520bbde1c80c684bfe428
achuluunkhuu <achuluunkhuu@ctacorp.com>
2018-02-26 16:09:22 -0500

    fix for https://ctac.myjetbrains.com/youtrack/issue/usagov-102117

--- sites/all/modules/custom/hooks_reaction/js/hide_ready_rp_state.js
+++ sites/all/modules/custom/hooks_reaction/js/hide_ready_rp_state.js
@@ -4,7 +4,7 @@
         if ($("#edit-workbench-moderation-state-new").length >0) {
             $("#edit-workbench-moderation-state-new option[value='scheduled_for_publication']").remove();
         }
-        
+
         if ($("#edit-state").length >0) {
             $("#edit-state option[value='scheduled_for_publication']").remove();
         }
