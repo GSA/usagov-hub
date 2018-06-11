22c3c9a3918987a9f0b27e068236e538d9123b31
achuluunkhuu <achuluunkhuu@ctacorp.com>
2018-06-11 15:28:02 -0400

    fix for https://ctac.myjetbrains.com/youtrack/issue/usagov-102138

--- sites/all/modules/custom/hooks_reaction/reactions/Taxonomy-LastEdit_Date-Helper.php
+++ sites/all/modules/custom/hooks_reaction/reactions/Taxonomy-LastEdit_Date-Helper.php
@@ -2,7 +2,12 @@
 
 hooks_reaction_add(array("hook_form_taxonomy_form_term_alter","hook_form_taxonomy_manager_form_alter"),
     function (&$form, &$form_state, $form_id) {
-        
+
+        if (isset($form['#entity']->tid)) {
+            $form['name']['#prefix'] = '<div class="form-item form-type-textfield form-item-name"><label for="edit-name">' . t("Taxonomy Term Id: ") .'</label>'. $form['#entity']->tid . '</div>';
+        }
+
+
         $changed = null;
         if ( !empty($form['#entity']) && isset($form['#entity']->changed) )
         {
@@ -13,11 +18,12 @@ hooks_reaction_add(array("hook_form_taxonomy_form_term_alter","hook_form_taxonom
         if ( $changed )
         {
             $convert_to_date= gmdate("m-d-Y", $changed);
-            $form['term_data']['last_chnaged_date'] = array(
-              '#type' => 'item',
+           /* $form['term_data']['last_chnaged_date'] = array(
+              '#type' => 'markup',
               '#title' => t("Last changed on"),
               '#markup' => $convert_to_date,
-              '#weight' => -9,
-            ); 
+              '#weight' => -80,
+            );*/
+            $form['name']['#prefix'] .= '<div class="form-item form-type-textfield form-item-name"><label for="edit-name">' . t("Last changed on: ") .'</label>'. $convert_to_date . '</div>';
         }
 });
