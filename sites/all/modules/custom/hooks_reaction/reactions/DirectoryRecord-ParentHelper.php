9c0b13c8441bba92342701fc7c6a7199f7914a08
achuluunkhuu <achuluunkhuu@ctacorp.com>
2019-02-21 14:57:37 -0500

    fix for https://ctac.myjetbrains.com/youtrack/issue/usagov-102304

--- sites/all/modules/custom/hooks_reaction/reactions/DirectoryRecord-ParentHelper.php
+++ sites/all/modules/custom/hooks_reaction/reactions/DirectoryRecord-ParentHelper.php
@@ -12,28 +12,27 @@
 
 $GLOBALS['DirRecordHelperParent_updateRefNid'] = null;
 $GLOBALS['DirRecordHelperParent_noPostProc'] = false;
+$GLOBALS['DirRecordHelperParent_noPreProc'] = false;
 $GLOBALS['DirRecordHelperParent_ignoreNids'] = array();
+$GLOBALS['DirRecordHelperParentPre_ignoreNids'] = array();
 
 hooks_reaction_add('HOOK_node_postsave',
     function ($node) {
-
         // We only care about Text-Asset nodes here
         if ( $node->type !== 'directory_record_content_type' ) {
             return;
         }
 
         // This is needed in order to prevent infinite loops (see later code)
-        if ( $GLOBALS['DirRecordHelperParent_noPostProc'] ) {
+        if ( $GLOBALS['DirRecordHelperParent_noPostProc'] || $GLOBALS['DirRecordHelperParent_noPreProc'] || $GLOBALS['DirRecordHelperChild_noPreProc'] || $GLOBALS['DirRecordHelperChild_noPostProc']) {
             return;
         }
-        if ( in_array(intval($node->nid), $GLOBALS['DirRecordHelperParent_ignoreNids']) ) {
+        if ( in_array(intval($node->nid), $GLOBALS['DirRecordHelperParent_ignoreNids']) ||  in_array(intval($node->nid), $GLOBALS['DirRecordHelperParentPre_ignoreNids']) ) {
             return;
         }
 
         $nodeNew = $node;
         $nodeOld = ( empty($node->original) ? false : $node->original );
-        dsm($nodeNew);
-        dsm($nodeOld);
 
         if ($nodeNew->field_parent_record_en['und'][0]['target_id'] != $nodeOld->field_parent_record_en['und'][0]['target_id']) {
 
@@ -41,11 +40,7 @@ hooks_reaction_add('HOOK_node_postsave',
                 // we found referenced node
                 $nodeRef = node_load($nodeNew->field_parent_record_en['und'][0]['target_id']);
             }
-            else{
-                $nodeOldParentRef = node_load($nodeOld->field_parent_record_en['und'][0]['target_id']);
-            }
 
-dsm($nodeOldParentRef);
             if (!empty($nodeRef)) {
                 $found = false;
 
@@ -80,30 +75,62 @@ dsm($nodeOldParentRef);
                 );
             }
 
+        }
+
+    } // End Hook: node_presave
+
+); // End hooks_reaction_add() call
+
+
+hooks_reaction_add('HOOK_node_presave',
+    function ($node) {
+        // We only care about Text-Asset nodes here
+        if ( $node->type !== 'directory_record_content_type' ) {
+            return;
+        }
+
+        // This is needed in order to prevent infinite loops (see later code)
+        if ( $GLOBALS['DirRecordHelperParent_noPostProc'] || $GLOBALS['DirRecordHelperParent_noPreProc'] || $GLOBALS['DirRecordHelperChild_noPreProc'] || $GLOBALS['DirRecordHelperChild_noPostProc']) {
+            return;
+        }
+        if ( in_array(intval($node->nid), $GLOBALS['DirRecordHelperParent_ignoreNids']) ||  in_array(intval($node->nid), $GLOBALS['DirRecordHelperParentPre_ignoreNids']) ) {
+            return;
+        }
+
+        $nodeNew = $node;
+        $nodeOld = ( empty($node->original) ? false : $node->original );
+
+
+        if ($nodeNew->field_parent_record_en['und'][0]['target_id'] != $nodeOld->field_parent_record_en['und'][0]['target_id']) {
+            if (!empty($nodeOld->field_parent_record_en['und'][0]['target_id'])) {
+                $nodeOldParentRef = node_load($nodeOld->field_parent_record_en['und'][0]['target_id']);
+            }
+
             // remove from children list if parent changes
             if(!empty($nodeOldParentRef)){
                 if (isset($nodeOldParentRef->field_child_records_en['und'])) {
                     $jj =0;
                     foreach ($nodeOldParentRef->field_child_records_en['und'] as $nchild) {
-                        $jj++;
                         if ($nchild['target_id'] == $nodeNew->nid) {
-                            unset($nodeOldParentRef->field_child_records_en['und'][$jj]);
+                            unset($nodeOldParentRef->field_child_records_en['und'][$jj]['target_id']);
                             break;
                         }
-                    }
-
-                    $jj = 0;
-                    foreach ($nodeOldParentRef->field_child_records_en['und'] as $nchild) {
                         $jj++;
-                        if (!empty($nchild)) {
-                            $nodeOldParentRef->field_child_records_en['und'][$jj][$nchild['target_id']] = $nchild['target_id'];
-
+                    }
+                    $jj = 0;
+                    $tmpchildren = $nodeOldParentRef->field_child_records_en['und'];
+                    unset($nodeOldParentRef->field_child_records_en['und']);
+                    foreach ($tmpchildren as $nchild) {
+                        if (!empty($nchild['target_id'])) {
+                            $nodeOldParentRef->field_child_records_en['und'][$jj]['target_id'] = $nchild['target_id'];
+                            $jj++;
                         }
                     }
-                    $GLOBALS['DirRecordHelperParent_noPostProc'] = true;
-                    $GLOBALS['DirRecordHelperParent_ignoreNids'][] = intval($nodeOldParentRef->nid);
+
+                    $GLOBALS['DirRecordHelperParent_noPreProc'] = true;
+                    $GLOBALS['DirRecordHelperParentPre_ignoreNids'][] = intval($nodeOldParentRef->nid);
                     node_save($nodeOldParentRef);
-                    $GLOBALS['DirRecordHelperParent_noPostProc'] = false;
+                    $GLOBALS['DirRecordHelperParent_noPreProc'] = false;
 
                 }
             }
@@ -112,3 +139,4 @@ dsm($nodeOldParentRef);
     } // End Hook: node_presave
 
 ); // End hooks_reaction_add() call
+
