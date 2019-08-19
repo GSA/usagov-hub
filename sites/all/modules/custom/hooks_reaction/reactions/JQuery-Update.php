07fbd6d256d5ee3030d71b67a61148773bcfd24c
achuluunkhuu <achuluunkhuu@ctacorp.com>
2019-08-19 15:29:28 -0400

    upgrading jQuery to be jquery-3.4.1

--- sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
+++ sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
@@ -4,8 +4,8 @@ hooks_reaction_add("js_alter",
     function (&$javascript) {
       if ( !user_is_logged_in() && !empty($javascript['misc/jquery.js']) )
       {
-        $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.2.1.min.js';
-        $javascript['misc/jquery.js']['version'] = '3.2.1';
+        $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.4.1.min.js';
+        $javascript['misc/jquery.js']['version'] = '3.4.1';
       }
     }
 );
