6322f707baa97f7ba5bcbc7df0f7e5c85cc20e97
Jeff Hendrickson <jhendrickson@ctacorp.com>
2019-08-21 11:59:43 -0400

    Incorporated changes from USA-59

--- sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
+++ sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
@@ -2,10 +2,14 @@
 
 hooks_reaction_add("js_alter",
     function (&$javascript) {
-      if ( !user_is_logged_in() && !empty($javascript['misc/jquery.js']) )
+      if ( !user_is_logged_in() && !empty($javascript['misc/jquery.min.js']) )
       {
-        $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.4.1.min.js';
-        $javascript['misc/jquery.js']['version'] = '3.4.1';
+        global $base_url;
+      	// JKH, no no, we're going to use a single js from jquery_update...
+        // $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.2.1.min.js';
+        // $javascript['misc/jquery.js']['version'] = '3.2.1';
+        $javascript['misc/jquery.min.js']['data'] = $base_url . '/misc/jquery.min.js';
+        $javascript['misc/jquery.min.js']['version'] = '3.4.1';
       }
     }
 );
