a5136aeddf063144772563955eb03827039280bf
Jeff Hendrickson <jeffrey.hendrickson@gsa.gov>
2019-10-02 14:38:14 -0400

    Ready for a second round of test

--- sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
+++ sites/all/modules/custom/hooks_reaction/reactions/JQuery-Update.php
@@ -6,8 +6,8 @@ hooks_reaction_add("js_alter",
       {
         global $base_url;
       	// JKH, no no, we're going to use a single js from jquery_update...
-        // $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.2.1.min.js';
-        // $javascript['misc/jquery.js']['version'] = '3.2.1';
+        // $javascript['misc/jquery.js']['data'] = drupal_get_path('module','hooks_reaction').'/js/jquery-3.4.1.min.js';
+        // $javascript['misc/jquery.js']['version'] = '3.4.1';
         $javascript['misc/jquery.min.js']['data'] = $base_url . '/misc/jquery.min.js';
         $javascript['misc/jquery.min.js']['version'] = '3.4.1';
       }
