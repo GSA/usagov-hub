be4cff34ce8b3427f88ad882cff77a2c463b105f
Jeff Hendrickson <jeffrey.hendrickson@gsa.gov>
2019-10-11 15:20:51 -0400

    Forgot to remove a few trace

--- sites/all/modules/custom/hooks_reaction/reactions/Taxonomy-Weight-Update.php
+++ sites/all/modules/custom/hooks_reaction/reactions/Taxonomy-Weight-Update.php
@@ -2,14 +2,14 @@
 hooks_reaction_add([
         "hook_taxonomy_term_validate"
     ],function ($term) {
-       tracetofile(__FILE__,__LINE__,"hook v");
+       // tracetofile(__FILE__,__LINE__,"hook v");
     }
 );
 
 hooks_reaction_add([
         "hook_taxonomy_term_delete"
     ],function ($term) {
-       tracetofile(__FILE__,__LINE__,"hook d");
+       // tracetofile(__FILE__,__LINE__,"hook d");
     }
 );
 
@@ -17,19 +17,19 @@ hooks_reaction_add([
         "hook_taxonomy_term_update",
         "hook_taxonomy_term_save"
     ],function ($term) {
-    	tracetofile(__FILE__,__LINE__,"hook u s");
+    	// tracetofile(__FILE__,__LINE__,"hook u s");
         // tracetofile(__FILE__,__LINE__,"===>hook u s p, let's look at term object");
         // traceobjects($term);
         // tracetofile(__FILE__,__LINE__,"===>end");
         // JKH added  
         if(!isset($term->parent) || empty($term->parent)) {
-        	tracetofile(__FILE__,__LINE__,"no parent!");
+        	// tracetofile(__FILE__,__LINE__,"no parent!");
         	drupal_set_message(t("You tried to save this taxonomy node without a parent node. Please assign a parent to this node before saving."), 'Error');
         	$curr_uri = check_plain(request_uri());
 			drupal_goto($curr_uri);        	
         }
-        tracetofile(__FILE__,__LINE__,"parent is set to ... ");
-        traceobjects($term->parent);
+        // tracetofile(__FILE__,__LINE__,"parent is set to ... ");
+        // traceobjects($term->parent);
         drupal_set_message(t(""));
         // JKH removed the lines that imposed condition on database write.
 		// ....
@@ -73,15 +73,15 @@ hooks_reaction_add([
         {
             $siblings[] = $siblingArray;
         }
-        tracetofile(__FILE__,__LINE__,"****siblings before sort");
-        traceobjects($siblings);
+        // tracetofile(__FILE__,__LINE__,"****siblings before sort");
+        // traceobjects($siblings);
         array_multisort(
             array_column($siblings,'weight'),SORT_ASC,
             array_column($siblings,'name'),  SORT_ASC,SORT_STRING|SORT_FLAG_CASE,
             array_column($siblings,'tid'),   SORT_ASC,
             $siblings);
-        tracetofile(__FILE__,__LINE__,"****siblings after sort");
-        traceobjects($siblings);
+        // tracetofile(__FILE__,__LINE__,"****siblings after sort");
+        // traceobjects($siblings);
         // convert these into one sql update query
         $sql_values = [];
         foreach ( $siblings as $weight=>&$sibling )
