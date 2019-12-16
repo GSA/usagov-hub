f732e15f281342ebed652f08a134035d720fc6d3
Jeff Hendrickson <jeffrey.hendrickson@gsa.gov>
2019-12-16 15:43:32 -0500

    First pass at taxonomy weight fix..., needs more testing/grooming

--- sites/all/modules/custom/hooks_reaction/reactions/Taxonomy-Weight-Update.php
+++ sites/all/modules/custom/hooks_reaction/reactions/Taxonomy-Weight-Update.php
@@ -3,25 +3,42 @@
 function react($term) {
 
 	// JKH added for debug...
-	// tracetofile(__FILE__,__LINE__,"term object");
+	// tracetofile(__FILE__,__LINE__,"react object");
 	// traceobjects($term);
+	
+	// JKH note: changed this function so that it actually 
+	// updates the weights of the numbers
+	$thisParent = 0;
 	// JKH added, per Joanne, we don't want the parent rule applied to ATT
 	// vid	name					machine_name			description	hierarchy	module		weight
 	// 57	Asset Topic Taxonomy	asset_topic_taxonomy				1			taxonomy	0
 	if($term->vid != 57) {  
+		// tracetofile(__FILE__,__LINE__,"get parents");
+		$parents = taxonomy_get_parents($term->tid); 
+		$numberOfParents = sizeof($parents);
+		// tracetofile(__FILE__,__LINE__,"number of parents " . $numberOfParents);
+		// traceobjects($parents);
 	    // tracetofile(__FILE__,__LINE__,"term is not ATT");
 		// if this is not ATT, make sure that it has a parent
-		if(!isset($term->parent) || empty($term->parent) || $term->parent==0) {
+		if($numberOfParents == 0) {
 			// tracetofile(__FILE__,__LINE__,"no parent!");
 			drupal_set_message(t("You tried to save this taxonomy node without a parent node. Please assign a parent to this node before saving."), 'Error');
 			$curr_uri = check_plain(request_uri());
 			drupal_goto($curr_uri);        	
+		} else {
+			// traceobjects($parents);
+			$numberOfParents = sizeof($parents);
+			foreach ($parents as $parent) {
+				$thisParent = $parent->tid;
+				break;
+			}
 		}
 	}
     // JKH added 
-    // tracetofile(__FILE__,__LINE__,"term has parent " . $term->parent);
+    tracetofile(__FILE__,__LINE__,"term has parent id " . $thisParent);
 	drupal_set_message(t(""));
 
+	// JKH using my varibles thisParent
 	// touch all siblings and update the changed time
 	// in the two tables that track these things
 	db_query("
@@ -30,60 +47,36 @@ function react($term) {
 		WHERE d.tid IN (
 			SELECT h.tid 
 			FROM {taxonomy_term_hierarchy} h
-			WHERE h.tid
-				AND h.parent = :parent
-		)
-	",[':parent'=>$term->parent]);
+			WHERE h.tid AND h.parent = " . $thisParent . ")");
 	db_query("
 		UPDATE {taxonomy_dates} d
 		SET d.changed = UNIX_TIMESTAMP(NOW())
 		WHERE d.tid IN (
 			SELECT h.tid 
 			FROM {taxonomy_term_hierarchy} h
-			WHERE h.tid
-				AND h.parent = :parent
-		)
-	",[':parent'=>$term->parent]);
+			WHERE h.tid AND h.parent = " . $thisParent . ")");
 
 	// recalculate weight for all effected items
+	// JKH order the data in the db query (vice sort)
 	$siblingResults = db_query("
 		SELECT 
-			d.tid, d.name, d.weight 
+			d.tid, d.name, d.weight, d.changed 
 		FROM 
 			{taxonomy_term_hierarchy} h
 			JOIN {taxonomy_term_data} d ON (d.tid = h.tid)
 		WHERE 
-			h.parent = :parent
-	",[':parent'=>$term->parent]);
-	// put these into array format so we can make user of _multisort func
-	$siblings = [];
-	while( $siblingArray = $siblingResults->fetchAssoc() )
+			h.parent = " . $thisParent . " 
+		ORDER BY d.changed DESC, d.weight ASC, d.name ASC");
+	$count = 0;
+	while($sibling = $siblingResults->fetchAssoc())
 	{
-		$siblings[] = $siblingArray;
+		tracetofile(__FILE__,__LINE__,"db_query updating sibling " . $sibling['name'] . " weight " . $count);
+		db_query("
+			UPDATE taxonomy_term_data
+			SET weight = " . $count++ . "
+			WHERE tid = " . $sibling['tid']);		
 	}
-	// tracetofile(__FILE__,__LINE__,"****siblings before sort");
-	// traceobjects($siblings);
-	array_multisort(
-		array_column($siblings,'weight'),SORT_ASC,
-		array_column($siblings,'name'),  SORT_ASC,SORT_STRING|SORT_FLAG_CASE,
-		array_column($siblings,'tid'),   SORT_ASC,
-		$siblings);
-	// tracetofile(__FILE__,__LINE__,"****siblings after sort");
-	// traceobjects($siblings);
-	// convert these into one sql update query
-	$sql_values = [];
-	foreach ( $siblings as $weight=>&$sibling )
-	{
-		$sql_values[] = "({$sibling['tid']},{$weight})";
-	}
-	db_query("
-		INSERT INTO {taxonomy_term_data} 
-			(tid,weight) 
-		VALUES 
-			".implode(',',$sql_values)." 
-		ON DUPLICATE KEY UPDATE 
-			weight=VALUES(weight)
-	");
+
 }
 
 // JKH added 
